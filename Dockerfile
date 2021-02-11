ad51763e774b18f352fded3b77dce04a743a5b4d
Canoso <jpcanoso@hotmail.com>
2021-02-11 19:37:27 +0000

    Added args to the Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM node:14-alpine
 
+ARG DB_USERNAME=root
+ARG DB_PASSWORD=teste
+ARG DB_DATABASE=mer
+ARG DB_HOST=localhost
+
 COPY ./src /merApi
 
 WORKDIR /merApi
--- src/config/config.js
+++ src/config/config.js
@@ -7,17 +7,17 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": "teste",
-    "database": "mer",
-    "host": "localhost",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": "teste",
-    "database": "database_production",
-    "host": "db",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   }
 };
